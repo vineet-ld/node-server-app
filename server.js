fe892efb458d382f26515cc645762567bf4144a4
Vineet Dandekar <vineet_ld@live.com>
2017-08-24 18:06:14 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("currentYear", () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", (text) => {
+    return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+
+    var log = `${new Date().toString()}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFileSync("server.log", log + "\n");
+    next();
+
+});
+
+app.use((req, res, next) => {
+
+    res.render("maintenance.hbs", {
+        pageTitle: "We are under Maintenance",
+        message: "See you soon"
+    });
+
+});
+
+app.get("/", (req, res) => {
+
+    res.render("home.hbs", {
+        pageTitle: "Home Page",
+        message: "Welcome"
+    });
+
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.hbs", {
+        pageTitle: "About Page"
+    });
+});
+
+app.get("/bad", (req, res) => {
+    res.send({
+        errorMessage: "Some error occured"
+    });
+});
+
+app.listen(3002, () => {
+    console.log("Server started at 3002");
+});
